9fb493535d67baddbc31e29f0ab7de16ad063a01
Law Kiang Hong K <kianghong.law@outlook.com>
2016-10-13 15:51:08 +0800

    Created Codes folder and added mobile application sources code

---
+++ Codes/DriverTrackingSystem_Mobile/app/src/main/java/com/example/kianghong/drivertrackingsystem_mobile/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.kianghong.drivertrackingsystem_mobile;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent browerIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://scoreboardsit.azurewebsites.net"));
+        startActivity(browerIntent);
+        finish();
+    }
+}
