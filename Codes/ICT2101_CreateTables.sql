9bf0f11beada4f2f4920a548a93d3f2ab1d2ea25
Law Kiang Hong K <kianghong.law@outlook.com>
2016-10-19 14:25:12 +0800

    Added CreateTable script and credentials to access sql server

---
+++ Codes/ICT2101_CreateTables.sql
@@ -0,0 +1,47 @@
+﻿-- Create table script for users
+Create Table Users
+(
+u_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+userName VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+role VARCHAR(100) NOT NULL,
+firstName VARCHAR(255),
+lastName VARCHAR(255),
+nric VARCHAR(20),
+contactNumber VARCHAR(20)
+)
+
+-- Create table script for vehicles
+-- Table is used to store information of the vehicle such as plate number, brand, model, year_of_purchase, type, driver
+Create Table Vehicles 
+(
+v_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+plateNumber VARCHAR(50) NOT NULL,
+brand VARCHAR(50) NOT NULL,
+model VARCHAR(100) NOT NULL,
+year_of_purchase VARCHAR(20),
+u_id INT NOT NULL FOREIGN KEY REFERENCES Users(u_id)
+)
+
+-- Create table script for packages
+-- Table is used to store information of the package such as order number, date of delivery, time of delivery, location, unit number, postal code
+Create Table Packages
+(
+p_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+order_Number VARCHAR(255) NOT NULL,
+delivery_Date DATETIME NOT NULL,
+location VARCHAR(255) NOT NULL,
+unitNumber VARCHAR(100),
+postalCode VARCHAR(10)
+)
+
+-- Create table script for road maps
+-- Table contain information on starting location, destination, vehicle that is sending the item, estimate time of delivery
+Create Table RoadMaps
+(
+r_id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+startLocation VARCHAR(255),
+p_id INT NOT NULL FOREIGN KEY REFERENCES Packages(p_id),
+eta INT,
+v_id INT NOT NULL FOREIGN KEY REFERENCES Vehicles(v_id)
+)
