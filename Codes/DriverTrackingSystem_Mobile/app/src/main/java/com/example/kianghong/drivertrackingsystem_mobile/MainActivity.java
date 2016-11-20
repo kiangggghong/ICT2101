2a4cac212da9886ad6f363e675a873d0b24adab8
Law Kiang Hong K <kianghong.law@outlook.com>
2016-11-21 00:15:16 +0800

    Make changes to the browser link

--- Codes/DriverTrackingSystem_Mobile/app/src/main/java/com/example/kianghong/drivertrackingsystem_mobile/MainActivity.java
+++ Codes/DriverTrackingSystem_Mobile/app/src/main/java/com/example/kianghong/drivertrackingsystem_mobile/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent browerIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+        Intent browerIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://ict2101-team41.azurewebsites.net"));
         startActivity(browerIntent);
         finish();
     }
